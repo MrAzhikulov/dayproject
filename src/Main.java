53c11813fdc6ca1ef6913509b464081db7908238
beknurkyyrgyzbaev <arapovbeknur@gmail.com>
2022-12-13 19:35:58 +0600

    f

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,19 @@ public class Main {
         Scanner console = new Scanner(System.in);
         int temp = console.nextInt();
         int age = console.nextInt();
-    public static void main(String[] args){
 
 
+
+    }
+    public static void temp(int temp) {
+        if (temp <0){
+            System.out.println("child wear a jacket ");
+        }
+        if(temp<=10 && temp>0) {
+            System.out.println("switer wear");
+        }
+        if (temp<10 ){
+            System.out.println("T-shirt");
+        }
     }
 }
