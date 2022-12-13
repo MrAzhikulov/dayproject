4404e4749a8d59b7904e24cba53a941851774745
sterben <mr.azhikulov@gmail.com>
2022-12-13 19:12:32 +0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int temp = console.nextInt();
+        int age = console.nextInt();
     }
 }
