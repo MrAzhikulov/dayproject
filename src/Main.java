feb784feb856743add0608aecdfd47740712d575
Nurbolsun-2022 <tajibaevnurbolsun2022@gmail.com>
2022-12-13 16:43:53 +0300

    k

--- src/Main.java
+++ src/Main.java
@@ -7,4 +7,15 @@ public class Main {
         int age = console.nextInt();
 
     }
+    public static void age(int age){
+        if (age<6){
+            System.out.println(" sadike bar");
+        }if (age>6 && age<=18){
+            System.out.println("mektepke bar");
+        }if (age>=18 && age<=24 ){
+            System.out.println("universitetke bar");
+        }if (age>24){
+            System.out.println("ishke bar");
+        }
+    }
 }
